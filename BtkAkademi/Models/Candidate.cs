2db1781cdea561d4804c8275f7036c081ce95ac3
A.Yusa Telli <yusahmetelli@gmail.com>
2025-01-20 15:08:31 +0300

    Add BTK Akademi files

---
+++ BtkAkademi/Models/Candidate.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        [Required(ErrorMessage ="Email is required.")]
+        public String? Email { get; set; } = String.Empty;
+        [Required(ErrorMessage ="First name is required.")]
+        public String? FirstName { get; set; } = String.Empty;
+        [Required(ErrorMessage ="Last name is required.")]
+        public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt {get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+    }
+}
